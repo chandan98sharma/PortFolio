9ebc229dbfe0a0799a194d881e009117402a908a
Chandan Sharma <2112chandansharma.com>
2020-03-30 19:30:51 +0530

    Deletes osp_da/book_store

--- osp_da/book_store/admin/includes/logo.inc.php
+++
@@ -1,1 +0,0 @@
-<marquee  direction="Left" behavior="alternate"><h1>Book Store</h1></marquee>
--- osp_da/book_store/includes/footer.inc.php
+++
@@ -1,3 +0,0 @@
-<div id="footer-wrap">
-	<p id="legal">(c) 2019 MySite. Designed by Chandan</p>
-	</div>
